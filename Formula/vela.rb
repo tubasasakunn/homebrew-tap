5709aff1832d5396f43fbced7c5e6b32351ff60c
tubasasakunn <wakaikitubasa@gmail.com>
2026-09-21 21:15:28 +0900

    feat: Vela 0.3.0へ更新

--- Formula/vela.rb
+++ Formula/vela.rb
@@ -1,8 +1,11 @@
 class Vela < Formula
   desc "File-configured macOS launcher, clipboard, hotkey, and window utility"
   homepage "https://github.com/tubasasakunn/vela"
-  url "https://github.com/tubasasakunn/vela/releases/download/v0.2.9/vela-0.2.9-darwin-arm64.tar.gz"
-  sha256 "fff52ad9b091e0b0431b3fc7b73f4033fb62fd1cb74d1ccf9cf8a4eca47bdcd5"
+  url "https://github.com/tubasasakunn/vela/releases/download/v0.3.0/vela-0.3.0-darwin-arm64.tar.gz"
+  sha256 "6adbb0f467d02b92de184c54e83432551ec5c07729387d5dd803e52478ddbc15"
+
+  depends_on arch: :arm64
+  depends_on macos: :sonoma
 
   def install
     bin.install "vela"
@@ -16,12 +19,8 @@ class Vela < Formula
 
   def caveats
     <<~EOS
-      Start Vela as a login service:
-        brew services start vela
-
-      Create and validate its Git-manageable configuration:
+      Start Vela's guided setup:
         vela init
-        vela check
 
       Grant Vela Accessibility access in System Settings for window actions.
     EOS
