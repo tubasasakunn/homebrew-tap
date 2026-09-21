13a3fc4dc12b64c9343b70823e6a8a75d98f6be4
tubasasakunn <wakaikitubasa@gmail.com>
2026-09-21 20:38:47 +0900

    feat: Vela 0.2.9 を配布

--- Formula/vela.rb
+++ Formula/vela.rb
@@ -1,9 +1,9 @@
 class Vela < Formula
   desc "File-configured macOS launcher, clipboard, hotkey, and window utility"
   homepage "https://github.com/tubasasakunn/vela"
-  url "https://github.com/tubasasakunn/vela/releases/download/v0.2.8/vela-0.2.8-darwin-arm64.tar.gz"
-  sha256 "abadbaed037b8e2df9d6a64379499c9cae34f0d945dc3da86b75d0ab5ea62ecc"
-  version "0.2.8"
+  url "https://github.com/tubasasakunn/vela/releases/download/v0.2.9/vela-0.2.9-darwin-arm64.tar.gz"
+  sha256 "fff52ad9b091e0b0431b3fc7b73f4033fb62fd1cb74d1ccf9cf8a4eca47bdcd5"
+  version "0.2.9"
 
   def install
     bin.install "vela"
