a72f27b57793986d70ca7a8cc10dd31d8c967199
tubasasakunn <wakaikitubasa@gmail.com>
2026-09-21 20:39:48 +0900

    fix: Vela formulaの監査を通す

--- Formula/vela.rb
+++ Formula/vela.rb
@@ -3,7 +3,6 @@ class Vela < Formula
   homepage "https://github.com/tubasasakunn/vela"
   url "https://github.com/tubasasakunn/vela/releases/download/v0.2.9/vela-0.2.9-darwin-arm64.tar.gz"
   sha256 "fff52ad9b091e0b0431b3fc7b73f4033fb62fd1cb74d1ccf9cf8a4eca47bdcd5"
-  version "0.2.9"
 
   def install
     bin.install "vela"
@@ -26,5 +25,9 @@ class Vela < Formula
 
       Grant Vela Accessibility access in System Settings for window actions.
     EOS
+  end
+
+  test do
+    assert_match "Usage: vela <command>", shell_output("#{bin}/vela")
   end
 end
